468ac3ba490da32f8afeb9bebc35b972ba5f6044
roseoutz <roseoutz@gmail.com>
2022-02-05 00:19:54 +0900

    2022.02.04 사용자 Common 으로 이동 JWT 서비스를 모든곳에서 이용할 수 있도록 라이브러리화

---
+++ btaka-jwt/src/test/java/com/btaka/BtakaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtakaJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
