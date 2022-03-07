6db50323d5fa1a7b4362a590950deb60ee302afe
roseoutz <roseoutz@gmail.com>
2022-03-07 23:27:28 +0900

    2022.03.07 게시판 계층형 대댓글 구현중

--- btaka-web-common/src/main/java/com/btaka/common/service/AbstractDataService.java
+++ btaka-web-common/src/main/java/com/btaka/common/service/AbstractDataService.java
@@ -3,6 +3,10 @@ package com.btaka.common.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+
 public abstract class AbstractDataService<T, D> {
 
     private final Class<T> entityClass;
@@ -15,6 +19,10 @@ public abstract class AbstractDataService<T, D> {
 
     @Autowired
     protected ModelMapper modelMapper;
+
+    protected String getUUID() {
+        return Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
+    }
 
     protected T toEntity(D dto) {
         return modelMapper.map(dto, entityClass);
