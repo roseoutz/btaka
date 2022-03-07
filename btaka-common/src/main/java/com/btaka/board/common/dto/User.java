e091a35ad7fabd8690ba29da7139f0ee80caf00b
roseoutz <roseoutz@gmail.com>
2022-03-07 17:21:19 +0900

    2022.03.07 사용자 로그아웃 구현

--- btaka-common/src/main/java/com/btaka/board/common/dto/User.java
+++ btaka-common/src/main/java/com/btaka/board/common/dto/User.java
@@ -3,6 +3,9 @@ package com.btaka.board.common.dto;
 import com.btaka.board.common.constants.Roles;
 import lombok.*;
 
+import java.beans.Transient;
+import java.time.LocalDateTime;
+
 
 @Builder
 @Data
@@ -13,6 +16,9 @@ public class User {
     private String oid;
     private String email;
     private String password;
+    private int failCount;
+    private boolean isLocked;
+    private LocalDateTime lockedTime;
     private String username;
     private String mobile;
     private String birthdate;
